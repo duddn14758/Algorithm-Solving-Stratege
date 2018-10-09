4aa339e3916199249c0e0a0a9a2800291a7ee365
DESKTOP-O257RHD\Youngwoo <duddn14758@gmail.com>
2018-10-10 04:20:28 +0900

    wrong answer

--- week03/Prob8/Prob8/소스.cpp
+++ week03/Prob8/Prob8/소스.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main() {
 	FILE *fp;
@@ -9,10 +10,14 @@ int main() {
 	char single='A';
 	char *arr;
 	int count[26] = { 0, };
-	int point[26] = { 0 };
+	int **point;
 	int max_length = 0;
 	int num_pointer;
 	bool multi = false;
+	int min_start=0;
+	clock_t start, end;
+
+	start = clock();
 
 	fscanf(fp, "%d", &case_num);
 
@@ -20,22 +25,51 @@ int main() {
 		fscanf(fp, "%d %d", &N, &K);
 		printf("%d %d\n",N,K);
 		arr = (char*)malloc(sizeof(char)*N);
+		point = (int**)malloc(sizeof(int*) * 26);
+		for (int j = 0; j < 26; j++) {
+			point[j] = (int*)malloc(sizeof(int)*K);
+		}
+		for (int j = 0; j < 26; j++) {
+			count[j] = 0;
+			for (int k = 0; k < K; k++) {
+				point[j][k] = 0;
+			}
+		}
+
 		fgetc(fp);
 		
 		for (int j = 0; j < N; j++) {
 			arr[j] = fgetc(fp);
 			num_pointer = arr[j] - 65;
-			if (count[num_pointer]++ == 0) {
-				point[num_pointer] = j;
+			if (count[num_pointer] < K+1) {		//count가 K+1보다 작을 시에는
+				point[num_pointer][count[num_pointer]++] = j;
 			}
-			if (count[num_pointer] <= K+1&&(j-point[num_pointer])>max_length) {
-				max_length = j - point[num_pointer];
-				printf("문자열 : %c, 시작 : %d, 마지막 : %d\n", num_pointer, num_pointer, j);
-				multi = true;
-			}
-			if (count[num_pointer] == K + 1) {
+			if (count[num_pointer] == K + 1) {			//숫자에 도달했을시 배열을 한행씩 밀고 maxcheck
+
+				for (int k = 0; k < 26; k++) {			// 만약 시작을 했는데 count가 K까지 도달하지 못한 알파벳이있다면 point를 민다
+					if (point[k][K - 1] == 0 && point[k][0] < min_start) {
+						int r = 0;
+						while(point[k][0]<point[num_pointer][0]&&r<K){
+							//printf("돈다아\n");
+							//printf("r : %d, point : %d\n", r,point[k][r]);
+							point[k][r++] = point[k][r];
+						}//j보다 작은동안 계속민다
+						point[k][r] = j;
+						count[k] -= r;
+					}						
+				}
+
+				min_start = point[num_pointer][0];
+							   
+				//printf("%d %d\n", min_start, j);
+				max_length = max_length > (j - min_start) ? max_length : (j - min_start);
+				
+				for (int k = 0; k < K-1; k++) {
+					point[num_pointer][k] = point[num_pointer][k + 1];
+				}
+				point[num_pointer][K - 1] = j;
 				count[num_pointer]--;
-				point[num_pointer] = j;
+				multi = true;
 			}
 		}
 		if (multi == false)
@@ -46,11 +80,14 @@ int main() {
 		max_length = 0;
 		for (int j = 0; j < 26; j++) {
 			count[j] = 0;
-			point[j] = 0;
+			for (int k = 0; k < K; k++) {
+				point[j][k] = 0;
+			}
 		}
 
 	}
-	
+	end = clock();
+	printf("걸린 시간 = %f초", (double)(end - start) / 1000);
 
 	return 0;
 }
