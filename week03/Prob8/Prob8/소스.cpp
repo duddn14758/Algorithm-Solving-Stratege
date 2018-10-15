407f13eeb7a22f571cc941200eed0c6398fcfdeb
Youngwoo <duddn14758@naver.com>
2018-10-15 17:33:23 +0900

    solve Prob8

--- week03/Prob8/Prob8/소스.cpp
+++ week03/Prob8/Prob8/소스.cpp
@@ -4,7 +4,7 @@
 
 int main() {
 	FILE *fp;
-	fp = fopen("input.txt", "r");
+	fp = fopen("input08.txt", "r");
 	int case_num;
 	int N, K;
 	char *arr;
@@ -13,6 +13,7 @@ int main() {
 	int max_length=0;
 	int head = 0, tail = 0;
 	clock_t start, end;
+	bool multi = false;
 
 	start = clock();
 
@@ -20,7 +21,6 @@ int main() {
 
 	for (int i = 0; i < case_num; i++) {
 		fscanf(fp, "%d %d", &N, &K);
-		printf("%d %d\n",N,K);
 		arr = (char*)malloc(sizeof(char)*N);
 		
 		for (int j = 0; j < 26; j++) {
@@ -33,37 +33,33 @@ int main() {
 			arr[j] = fgetc(fp);
 			num_pointer = arr[j] - 65;
 
-			if (count[num_pointer] < K) {		//count가 K보다 작을 시에 head 전진				
+			if (count[num_pointer] < K) {			
 				count[num_pointer]++;		//count에 ++
 			}
 			else if (count[num_pointer] >= K) {		//count가 K보다 크거나 도달했을시에
+				multi = true;
 				if (max_length < j - tail) {
-					//printf("j : %d, TAIL : %d", j, tail);
 					max_length = j - tail;
-					printf("max change %d\n", max_length);
 				}
 				while (arr[tail] != arr[j]&&tail<j) {	//해당 문자가 나올 때 까지 tail을 증가시킨다.
 					count[arr[tail] - 65]--;
 					tail++;
-				//printf("tail 증가 : %d, now j : %d, %c\n", tail,j,arr[tail]);
 				}				
-				printf("%c %c\n", arr[tail], arr[j]);
 				tail++;			// 해당 문자에 도달했다면 tail을 한번 더 ++
-				count[num_pointer]--;
-				printf("마지막에서 tail 증가 : %d, %c\n", tail, arr[tail]);
 			}
 			if (max_length < j - tail) {
-				printf("j : %d, TAIL : %d", j, tail);
 				max_length = j - tail;
-				printf("max change %d\n", max_length);
-			}
-			
-			//printf("HEAD : %d, TAIL : %d\n", head, tail);
+			}			
 		}		
-		//printf("HEAD : %d, TAIL : %d\n", head, tail);
+
+		if (multi == false) {
+			max_length = N;
+		}
+		
 		printf("max length : %d\n",max_length);
 		max_length = 0;
 		head = 0;
+		multi = false;
 		tail = 0;
 		for (int j = 0; j < 26; j++) {
 			count[j] = 0;
