0103b15764f12faf833fadb5a69326e48b06acef
Youngwoo <duddn14758@naver.com>
2018-11-13 22:27:22 +0900

    prob13 almost solved

--- week06/Prob13/Prob13/Prob13.c
+++ week06/Prob13/Prob13/Prob13.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <math.h>
 
 int **arr;
 int *visited;
@@ -17,10 +18,9 @@ int main() {
 	while (T--) {
 		fscanf(fp, "%d %d", &N, &K);
 		arr = (int**)malloc(sizeof(int*)*N);
-		visited = (int*)malloc(sizeof(int)*N);
+		visited = (int*)malloc(sizeof(int)*K);
 		
 		for (int i = 0; i < N; i++) {
-			visited[i] = 0;
 			fscanf(fp, "%d", &buffer);
 			fscanf(fp, "%d", &buffer);
 			fscanf(fp, "%d", &buffer);
@@ -30,6 +30,9 @@ int main() {
 			for (int j = 1; j <= number; j++) {
 				fscanf(fp, "%d", &arr[i][j]);
 			}
+		}
+		for (int i = 0; i < K; i++) {
+			visited[i] = -1;
 		}
 		printf("%d", getCycle(N));
 	}
@@ -49,21 +52,27 @@ int getCycle(int N) {
 int Cycle(int start, int cur,int count) {
 	
 	if (start == cur && count == K) {
-		printf("current : %d, count : %d\n",cur,count);
 		return 1;
-	}
-	else if (visited[cur] == 1) {
-		return 0;
-	}
+	}	
 	else if (count >= K)
 		return 0;
+	
+	for (int i = 0; i < count; i++) {
+		if (visited[i] == cur)
+			return 0;
+		if(i<count-1)
+			for (int j = 1; j < arr[cur][0]; j++) {
+				if (arr[cur][j] == visited[i])
+					return 0;
+			}
+	}
 
-	visited[cur] = 1;
+	visited[count] = cur;
 	
 	int num = 0;
 	for (int i = 1; i <= arr[cur][0]; i++) {
 		num += Cycle(start, arr[cur][i] - 1, count + 1);
 	}
-	visited[cur] = 0;
+	visited[count] = -1;
 	return num;
 }
