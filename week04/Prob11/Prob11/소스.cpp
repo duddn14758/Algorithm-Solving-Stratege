247ae16cadc03b2e785732548e5329b7df7e28f8
Youngwoo <duddn14758@naver.com>
2018-11-21 22:10:51 +0900

    commit

--- week04/Prob11/Prob11/소스.cpp
+++ week04/Prob11/Prob11/소스.cpp
@@ -1,11 +1,22 @@
 #include <stdio.h>
 
+typedef struct point {
+	int x;
+	int y;
+}Point;
+
+Point pt[81] = { 0,0 };
 int arr[9][9];
 int vari[9][9][9];
 int varic[9][9];
 bool setNum(int row, int col);
 void setRCS(int row, int col, int number);
 int count = 0;
+int pointer = 0;
+bool chk_row[9];
+bool chk_col[9];
+bool chk_round[3][3];
+
 
 int main() {
 
@@ -16,25 +27,40 @@ int main() {
 			scanf("%d", &arr[i][j]);
 			varic[i][j] = 0;
 			if (arr[i][j] == 0) {
+				pt[pointer].x = i;
+				pt[pointer++].y = j;
+				chk_row[i] = true;
+				chk_col[j] = true;
+				chk_round[i / 3][j / 3] = true;
+				//printf("%d %d %d\n", pt->x, pt->y, pointer);
 				count++;
 				for (int k = 0; k < 9; k++)
 					vari[i][j][k] = 0;
 			}
+			else {
+				chk_row[i] = false;
+				chk_col[j] = false;
+				chk_round[i / 3][j / 3] = false;
+			}			
 		}
 
-
 	while (count > 0) {
+		for (int i = 0; i < pointer; i++) {
+			if (setNum(pt[i].x, pt[i].y)) {
+				count--;
+				//setRCS(pt[i].x, pt[i].y, arr[pt[i].x][pt[i].y]);
+			}			
+		}
 
-		for (int i = 0; i < 9; i++) {
+		/*for (int i = 0; i < 9; i++) {
 			for (int j = 0; j < 9; j++) {
 				if (arr[i][j] == 0)
 					if (setNum(i, j)) {
 						count--;
 						setRCS(i, j, arr[i][j]);
-						//다읽어서 0 찾지말고 포인트를 찍어두고 찾으면 더빠르지않을까..
 					}
 			}
-		}
+		}*/
 	}
 
 	for (int i = 0; i < 9; i++) {
@@ -58,6 +84,7 @@ void setRCS(int row, int col, int number) {
 	int rstand = row-row%3;
 	int cstand = col-col%3;
 
+	if(chk_row[row]||chk_col[col]||chk_round[row/3][col/3])
 	for (int i = 0; i < 9; i++) {
 		if (varic[row][i] == 2) {
 			count--;
