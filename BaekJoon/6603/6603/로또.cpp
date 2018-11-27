e9796ad705979c43b1853e2e4981771a09a5631c
Youngwoo <duddn14758@naver.com>
2018-11-28 03:39:47 +0900

    6603 finish

---
+++ BaekJoon/6603/6603/로또.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+void printCase(int pointer, int count);
+
+int num;
+int buffer[6];
+int arr[13];
+
+
+int main() {
+	int stop;
+
+	while (1) {
+		scanf("%d", &num);
+		if (!num)
+			break;
+
+		for (int i = 0; i < num; i++) {
+			scanf("%d", &arr[i]);
+		}
+
+		for (int i = 0; i <= num-6; i++)
+			printCase(i, 0);
+		
+		printf("\n");
+	}
+
+
+	return 0;
+}
+
+void printCase(int pointer, int count) {
+	if (pointer > num || count > 6)
+		return;
+	
+	buffer[count] = arr[pointer];	
+
+	if (count == 5) {
+		for (int i = 0; i < 6; i++)
+			printf("%d ", buffer[i]);
+		printf("\n");
+	}
+
+	for (int i = pointer+1; i < num; i++) {
+		printCase(i, count + 1);
+	}
+}
