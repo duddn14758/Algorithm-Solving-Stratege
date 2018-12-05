9391f30c821132b5a9420dbca2eda47afe400bc0
Youngwoo <duddn14758@naver.com>
2018-12-05 20:30:12 +0900

    1149 finish

--- BaekJoon/1149/1149/RGB거리.cpp
+++ BaekJoon/1149/1149/RGB거리.cpp
@@ -1,31 +1,36 @@
 #include <stdio.h>
 
-int min = 0;
 int N;
 int color[1001][3];
+int cost[1001][3];
 
 int main() {
 
 	scanf("%d", &N);
 
 	for (int i = 0; i < N; i++)
-		for (int j = 0; j < 3; j++)
+		for (int j = 0; j < 3; j++) {
 			scanf("%d", &color[i][j]);
-	
-	
-	
+			if (i == 0)
+				cost[i][j] = color[i][j];
+			else {
+				switch (j) {
+				case 0:
+					cost[i][j] = cost[i - 1][1] < cost[i - 1][2] ? cost[i - 1][1] : cost[i - 1][2];
+					break;
+				case 1:
+					cost[i][j] = cost[i - 1][0] < cost[i - 1][2] ? cost[i - 1][0] : cost[i - 1][2];
+					break;
+				case 2:
+					cost[i][j] = cost[i - 1][0] < cost[i - 1][1] ? cost[i - 1][0] : cost[i - 1][1];
+					break;
+				}
+				cost[i][j] += color[i][j];
+			}
+		}
+	int min = cost[N - 1][0] < cost[N - 1][1] ? cost[N - 1][0] : cost[N - 1][1];
+	min = min < cost[N - 1][2] ? min : cost[N - 1][2];
+	printf("%d\n", min);
 
 	return 0;
-}
-
-void draw(int prev, int count,int price) {
-	if (count == N) {
-		if (min == 0||price<min)
-			min = price;		
-	}
-	else
-		for (int i = 0; i < 3; i++) {
-			if (i != prev)
-				draw(i, count + 1, price + color[count][i]);
-		}
 }
