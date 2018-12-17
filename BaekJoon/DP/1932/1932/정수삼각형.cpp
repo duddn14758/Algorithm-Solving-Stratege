b02be12d94e8a8dba27223b3768064d1eeb5ca87
duddn14758 <duddn14758@naver.com>
2018-12-17 18:09:50 +0900

    1032 ing

--- BaekJoon/DP/1932/1932/정수삼각형.cpp
+++ BaekJoon/DP/1932/1932/정수삼각형.cpp
@@ -9,14 +9,14 @@ int main() {
 	scanf("%d", &num);
 
 	scanf("%d", &tree[0][0]);
-	scanf("%d", &dp[0][0]);
+	dp[0][0] = tree[0][0];
 
 	for (int i = 1; i < num; i++) {
 		for (int j = 0; j <= i; j++) {
 			scanf("%d", &tree[i][j]);
 			if (j == 0) dp[i][j] = tree[i - 1][j];
 			else if (i == j) dp[i][j] = tree[i - 1][j - 1];
-			else dp[i][j] = dp[i - 1][j] > dp[i][j + 1] ? dp[i - 1][j] : dp[i - 1][j + 1];
+			else dp[i][j] = dp[i - 1][j] > dp[i-1][j + 1] ? dp[i - 1][j] : dp[i - 1][j + 1];
 			dp[i][j] += tree[i][j];
 			printf("%d ", dp[i][j]);
 		}
