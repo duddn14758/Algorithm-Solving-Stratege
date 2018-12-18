0cddc8805b9b2a1bb34fee6344cb79bb2c7b6a0a
duddn14758 <duddn14758@naver.com>
2018-12-18 22:31:30 +0900

    2583 ing

---
+++ BaekJoon/DFS/2583/2583/영역구하기.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	int x, y, num;
+	int lx, ly, rx, ry;
+
+	scanf("%d %d %d", &x, &y, &num);
+
+
+	for (int i = 0; i < num; i++) {
+		scanf("%d %d %d %d", &lx, &ly, &rx, &ry);
+	}
+	
+
+	return 0;
+}
