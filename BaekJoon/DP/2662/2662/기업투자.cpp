0e41d09c5b66838f97e179bbd38af156340e3d46
Youngwoo <duddn14758@naver.com>
2019-01-09 04:34:26 +0900

    2662 ing2

--- BaekJoon/DP/2662/2662/기업투자.cpp
+++ BaekJoon/DP/2662/2662/기업투자.cpp
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
-int chart[301][21];
-int dp[301];
-int invest[21];
+void getDP(int count);
+
+int chart[301][22];
+int dp[301][21];
+int company[301][21];
 int N, M;
 
 int main() {
@@ -15,17 +17,51 @@ int main() {
 	for (int i = 0; i < N; i++)
 		getDP(i);
 
+	/*printf("%d\n", dp[N-1][M-1]);
+	for (int i = 0; i < M; i++) {
+		printf("%d ", company[N - 1][i]);
+	}
+
+	printf("\n");*/
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			printf("%d ", dp[i][j]);
+		}
+		printf("\n");
+	}
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			printf("%d ", company[i][j]);
+		}
+		printf("\n");
+	}
 
 	return 0;
 }
 
 void getDP(int count) {
-	if (count == 0)
-		for (int i = 1; i <= M; i++)
-			dp[count] = dp[count] > chart[count][i] ? dp[count] : chart[count][i];
-	else
-		for (int i = 0; i < M; i++) {
-
+	
+		for (int i = 1; i <= M; i++) {
+			if (i == 1) {
+				dp[count][0] = chart[count][1];
+				company[count][i - 1] = count + 1;
+			}
+			else {
+				if (dp[count][i - 2] <= chart[count][i]) {
+					for (int j = 0; j < i; j++)
+						company[count][j] = 0;
+					company[count][i-1] = count + 1;
+				}
+				dp[count][i-1] = dp[count][i-2]>chart[count][i]?dp[count][i-2]:chart[count][i];
+				for (int j = 0; j < count; j++) {	
+					if (dp[count - j - 1][i - 2] + chart[j][i] > dp[count][i - 1]) {
+						for(int k=0;k<i;k++)
+							company[count][k] = company[count - j - 1][k];
+						company[count][i - 1] = j;
+					}
+					dp[count][i - 1] = dp[count - j -1][i - 2] + chart[j][i] > dp[count][i - 1] ? dp[count - j - 1][i - 2] + chart[j][i] : dp[count][i - 1];
+				}
+			}
 		}
-
 }
