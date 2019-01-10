5667217eb36e834ab95a0c7de61b320afbb95529
duddn14758 <duddn14758@naver.com>
2019-01-10 18:22:25 +0900

    not solved..

--- BaekJoon/DP/2662/2662/기업투자.cpp
+++ BaekJoon/DP/2662/2662/기업투자.cpp
@@ -4,7 +4,8 @@ void getDP(int count);
 
 int chart[301][22];
 int dp[301][21];
-int company[301][21];	//22 행렬마다 케이스 설정?
+int company[301][21][21];	//22 행렬마다 케이스 설정?
+int comp[21];
 int N, M;
 
 int main() {
@@ -17,11 +18,11 @@ int main() {
 	for (int i = 0; i < N; i++)
 		getDP(i);
 
-	/*printf("%d\n", dp[N - 1][M - 1]);
+	printf("%d\n", dp[N - 1][M - 1]);
 	for (int i = 0; i < M; i++)
-		printf("%d ", company[N-1][i]);*/
+		printf("%d ", company[N-1][M-1][i]);
 
-	for (int i = 0; i < N; i++) {
+	/*for (int i = 0; i < N; i++) {
 		for (int j = 0; j < M; j++) {
 			printf("%d ", dp[i][j]);
 		}
@@ -34,7 +35,7 @@ int main() {
 			printf("%d ", company[i][j]);
 		}
 		printf("\n");
-	}
+	}*/
 
 	return 0;
 }
@@ -44,20 +45,20 @@ void getDP(int count) {
 		for (int i = 1; i <= M; i++) {
 			if (i == 1) {
 				dp[count][0] = chart[count][1];
-				company[count][i - 1] = count + 1;
+				company[count][i - 1][0] = count + 1;
 			}
 			else {
 				if (dp[count][i - 2] <= chart[count][i]) {
-					for (int j = 0; j < i; j++)
-						company[count][j] = 0;
-					company[count][i-1] = count + 1;
+					for (int j = 0; j < i-1; j++)
+						company[count][i-1][j] = 0;
+					company[count][i - 1][i - 1] = count + 1;
 				}
 				dp[count][i-1] = dp[count][i-2]>chart[count][i]?dp[count][i-2]:chart[count][i];
 				for (int j = 0; j < count; j++) {	
 					if (dp[count - j - 1][i - 2] + chart[j][i] > dp[count][i - 1]) {
-						for(int k=0;k<i;k++)
-							company[count][k] = company[count - j - 1][k];
-						company[count][i - 1] = chart[j][0];
+						for (int k = 0; k < i; k++)
+							company[count][i - 1][k] = company[j][i - 2][k];
+						company[count][i - 1][i - 1] = count - j ;
 					}
 					dp[count][i - 1] = dp[count - j -1][i - 2] + chart[j][i] > dp[count][i - 1] ? dp[count - j - 1][i - 2] + chart[j][i] : dp[count][i - 1];
 				}
