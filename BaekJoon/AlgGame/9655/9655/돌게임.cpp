bef3c18992e7987cb1fae3633cdbb7ca322b8f1f
DESKTOP-O257RHD\Youngwoo <duddn14758@gmail.com>
2019-01-12 00:12:01 +0900

    hearthStone difficult

--- BaekJoon/AlgGame/9655/9655/돌게임.cpp
+++ BaekJoon/AlgGame/9655/9655/돌게임.cpp
@@ -1,33 +1,28 @@
 #include <stdio.h>
 
-bool win(int a);
-
-int arr[3] = { 1,3,4 };
+bool winArr[1001];
 
 int main() {
 	int a;
-	int player = 0;
 
-	while (1) {
+	scanf("%d", &a);
 
-		scanf("%d", &a);
+	winArr[1] = 1;
+	winArr[2] = 0;
+	winArr[3] = 1;
+	winArr[4] = 1;
 
-		if (win(a))
-			printf("SK");
+	for (int i = 5; i <= a; i++) {
+		if (winArr[i-1]&& winArr[i - 3]&& winArr[i - 4])
+			winArr[i] = false;
 		else
-			printf("CY");
+			winArr[i] = true;
 	}
-	return 0;
-}
 
-bool win(int a) {
-	if (a < 0)
-		return false;
-	if (a -arr[0]==0||a-arr[1]==0||a-arr[2]==0)
-		return true;
-	for (int i = 0; i < 3; i++) {
-		if (win(a - arr[i] - arr[0]) && win(a - arr[i] - arr[1]) && win(a - arr[i] - arr[2]))
-			return true;
-	}
-	return false;
+	if (winArr[a])
+		printf("SK");
+	else
+		printf("CY");
+	
+	return 0;
 }
