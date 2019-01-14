e61619e470a3427c74d59639fd31f4e099cb383a
DESKTOP-O257RHD\Youngwoo <duddn14758@naver.com>
2019-01-15 01:50:01 +0900

    9658 fin

---
+++ BaekJoon/AlgGame/9658/9658/돌게임4.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+	int N;
+	bool win[1005];
+
+	scanf("%d", &N);
+
+	win[1] = false;
+	win[2] = true;
+	win[3] = false;
+	win[4] = true;
+	win[5] = true;
+
+	for (int i = 5; i <= N; i++) {
+		if (!win[i - 1] || !win[i - 3] || !win[i - 4]) {
+			win[i] = true;
+		}
+		else
+			win[i] = false;
+	}
+
+	if (win[N])
+		printf("SK");
+	else
+		printf("CY");
+
+	return 0;
+}
