439a23ad284b0b5c5edee000d5cb56c8375e93d1
DESKTOP-O257RHD\Youngwoo <duddn14758@naver.com>
2019-01-14 22:37:11 +0900

    1016 diffffffficult...

--- BaekJoon/Math/1016/1016/제곱ㄴㄴ수.cpp
+++ BaekJoon/Math/1016/1016/제곱ㄴㄴ수.cpp
@@ -1,45 +1,28 @@
 #include <stdio.h>
 
 int main() {
-	unsigned long long min,max;
-	unsigned long long count = 0;
-	bool prime[1000001];
-	bool nono;
+	long long min, max;
+	int count = 0;
+	bool nono[1000001];
 
 	scanf("%lld %lld", &min, &max);
 
-	for (int i = 2; i < 1000001; i++) {
-		if (!prime[i]) 
-			continue;
-		else {
-			for (int j = i + i; j < 1000001; j +=i) {
-				/*if ((j>=min&&j<=max)||(j*j >= min && j*j <= max))
-					count++;*/
-				prime[j] = false;
-			}
-		}
+	for (int i = 0; i <= 1000000; i++)
+		nono[i] = false;
+		
+	for (long long i = 2; i*i <= max; i++) {
+		long long start = min / (i*i);
+		if (start*i*i != min)start++;
+		for (long long j = start; i*i*j<=max;j++) {
+			nono[i*i*j - min] = true;
+		}		
 	}
 
-	//printf("%lld\n", max - min - count);
-
-	for (unsigned long long i = min; i <= max; i++) {
-		nono = true;
-		for (unsigned long long j = 2; j < 1000001; j++) {
-			if (!prime[j])
-				continue;
-			else {
-				if (i % (j*j) == 0) {
-					nono = false;
-					break;
-				}
-			}
-		}
-		if (nono) {
+	for (int i = 0; i <= max - min; i++)
+		if(!nono[i])
 			count++;
-		}
-	}
-
-	printf("%lld\n", count);
+	
+	printf("%d\n", count);
 
 	return 0;
 }
