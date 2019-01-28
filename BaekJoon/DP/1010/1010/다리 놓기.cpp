eddbabca02ee6c6843b09b193ceaa46b9192a888
duddn14758 <duddn14758@naver.com>
2019-01-28 15:29:26 +0900

    solve 1904 dp

--- BaekJoon/DP/1010/1010/다리 놓기.cpp
+++ BaekJoon/DP/1010/1010/다리 놓기.cpp
@@ -1,39 +1,23 @@
 #include <iostream>
 
-int com(int a, int b);
-
-int dp[31];
+int dp[35][35];
 
 int main() {
 	int c,l,r;
 	std::cin >> c;
 
+	for (int i = 1; i < 35; i++) {
+		dp[i][i] = 1;
+		for (int j = i+1; j < 35; j++) {
+			if (i == 1) dp[i][j] = j;
+			else dp[i][j] = dp[i][j - 1] + dp[i - 1][j - 1];
+		}
+	}
+
 	while (c-- > 0) {
 		std::cin >> l >> r;
-		dp[l] = 1;
-		for (int i = l+1; i <= r; i++) {
-			int sum = 0;
-			for (int j = 0; j < i - l; j++) {
-				sum+= com(i - l + 1, i-j);
-			}
-			dp[i] = dp[i - 1] + sum;
-			//dp[i] = dp[i - 1] + com(i - l+1, i);
-			//std::cout << "dp = " << dp[i] << std::endl;
-		}
-		std::cout << dp[r] << std::endl;
+		std::cout << dp[l][r] << std::endl;
 	}
 
 	return 0;
-}
-
-int com(int b, int a) {
-	int fac = 1;
-	for (int i = 0; i < b;i++) {
-		fac *= a - i;
-	}
-	for (int i = 1; i <= b; i++) {
-		fac /= i;
-	}
-	//std::cout << "return " << fac << std::endl;
-	return fac;
 }
