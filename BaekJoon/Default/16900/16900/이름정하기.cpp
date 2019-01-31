d6d9450f333f445a5577474c777a4e7ec15c8f3d
DESKTOP-O257RHD\Youngwoo <duddn14758@naver.com>
2019-02-01 02:48:15 +0900

    .

--- BaekJoon/Default/16900/16900/이름정하기.cpp
+++ BaekJoon/Default/16900/16900/이름정하기.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
 
+char name[500002];
+
 int main() {
-	char name[500002];
 	long long num;
 	std::cin >> name>>num;
-	long long cur=1,pointer = 0;
-
+	long long cur = 1, pointer = 0;	
+	
 	while (name[cur] >= 'a'&&name[cur] <= 'z') {
 		if (name[cur] == name[pointer]) pointer++;
-		else {		
+		else {
 			pointer = 0;
 			if (name[cur] == name[pointer]) pointer++;
 		}
 		cur++;
-	}	
-	//std::cout << cur*num-(num-1)*pointer;
-	std::cout << cur+(cur-pointer)*(num-1);
+	}
+	std::cout << cur + (cur - pointer)*(num - 1);
 
 	return 0;
 }
