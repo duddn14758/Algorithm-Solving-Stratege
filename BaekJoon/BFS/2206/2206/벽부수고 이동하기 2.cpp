e9b4aa0693b2768c63c0137b91c988e8fa168609
duddn14758 <duddn14758@naver.com>
2019-02-18 18:43:54 +0900

    2206 practice BFS

--- BaekJoon/BFS/2206/2206/벽부수고 이동하기 2.cpp
+++ BaekJoon/BFS/2206/2206/벽부수고 이동하기 2.cpp
@@ -1,52 +1,72 @@
 #include <stdio.h>
 #include <queue>
 
+using namespace std;
 // using BFS
 
 bool maze[1001][1001];
-bool visited[1001][1001];
-int n, m, min = 1000000;
+bool visited[1001][1001][2];
+int n, m;
+int dx[] = { 0,0,1,-1 };
+int dy[] = { 1,-1,0,0 };
 
-bool canGo(int x, int y, bool bk,int cnt) {
-	if (x<0 || y<0 || x>n - 1 || y>m - 1 || cnt >= min || visited[x][y] || (!bk&maze[x][y]))
+bool canGo(int x, int y) {
+	if (x<0 || y<0 || x>n - 1 || y>m - 1)
 		return 0;
 	return 1;
-}
-
-void BFS(int x, int y, bool bk, int cnt) {
-	if (x == n - 1 && y == m - 1) {
-		min = min < cnt ? min : cnt;
-		return;
-	}
-	visited[x][y] = 1;
-
-	std::queue<std::pair<int,int>> q;
-	if (maze[x][y]) bk = 0;
-	if (canGo(x + 1, y, bk, cnt)) q.push({ x + 1,y });
-	if (canGo(x - 1, y, bk, cnt)) q.push({ x - 1,y });
-	if (canGo(x, y + 1, bk, cnt)) q.push({ x,y + 1 });
-	if (canGo(x, y - 1, bk, cnt)) q.push({ x,y - 1 });
-	while (!q.empty()) {
-		BFS(q.front().first, q.front().second, bk, cnt+1);
-		q.pop();
-	}
-	
-	visited[x][y] = 0;
 }
 
 int main() {
 	scanf("%d %d", &n, &m);
 	char buf[1001];
+	std::queue<std::pair<std::pair<int, int>,int>> q;
+	int result = 1;
+	
 	for (int i = 0; i < n; i++) {
 		scanf("%s", buf);
 		for (int j = 0; j < m; j++) {
 			maze[i][j] = buf[j] - '0';
 		}
 	}
-	BFS(0, 0, 1, 1);
 
-	if (min == 1000000) min = -1;
-	printf("%d", min);
+	q.push(make_pair(make_pair(0,0),0));
+	visited[0][0][0] = 1;
+	while (!q.empty()) {
+		int qsize = q.size();
+		pair<pair<int, int>,int> cur;
+		bool reach = false;
+		for (int i = 0; i < qsize; i++) {
+			cur = q.front();
+			if (cur.first.first == n - 1 && cur.first.second == m - 1) {
+				reach = true;
+				break;
+			}
 
+			q.pop();
+			for (int i = 0; i < 4; i++) {
+				int x = cur.first.first + dx[i];
+				int y = cur.first.second + dy[i];
+				if (!canGo(x, y)) continue;
+				if (visited[x][y][cur.second]) continue;
+				if (maze[x][y]) {
+					if (cur.second) continue;
+					visited[x][y][1] = true;
+					q.push(make_pair(make_pair(x, y), 1));
+				}
+				else {
+					visited[x][y][cur.second] = true;
+					q.push(make_pair(make_pair(x, y), cur.second));
+				}
+			}
+		}
+		if (reach)
+			break;
+		result++;
+	}
+	if (visited[n-1][m-1][0]|visited[n-1][m-1][1])
+		printf("%d", result);
+	else
+		printf("-1");
+	
 	return 0;
 }
