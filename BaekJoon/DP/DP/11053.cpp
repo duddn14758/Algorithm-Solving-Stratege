7c15a624e6c0a43da555f04ff6172951f51ac135
DESKTOP-O257RHD\Youngwoo <duddn14758@naver.com>
2019-03-01 03:45:10 +0900

    solve 11053 by dp

--- BaekJoon/DP/DP/11053.cpp
+++ BaekJoon/DP/DP/11053.cpp
@@ -1,27 +1,30 @@
 #include <iostream>
+#include <vector>
 
 // 가장 긴 증가하는 부분
 using namespace std;
 
-int dp[1001];
 int arr[1001];
-
-int getCloseSmall(int n) {
-	for (int i = n-1; i >= 0; i--) {
-		if (arr[i] < arr[n])
-			return i;
-	}
-	return -1;
-}
+int dp[1001];
 
 int main() {
-	int n;
+	int n, min_num,max_num=0;
 	cin >> n;
-	for (int i = 0; i < n; i++) {
+	for (int i = 1; i <= n; i++) 
 		cin >> arr[i];
-		if (i == 0) dp[i] = 1;
-		else dp[i]=d
-	}
+	dp[0] = 0;
+	for (int i = 1; i <= n; i++) {
+		min_num = 0;
+		for (int j = 0; j < i; j++) {	// 자기보다 작은 값들 중 dp가 최대인 값 + 1
+			if (arr[i] > arr[j]) {
+				min_num = dp[j] > min_num ? dp[j] : min_num;
+			}
+		}
+		dp[i] = min_num + 1;
+		max_num = dp[i] > max_num ? dp[i] : max_num;
+		//cout << max_num << " ";
+	}		
+	cout << max_num;
 
 	return 0;
 }
