0b74c36fd47f3d4c9d71c8be30806d35d0e87740
duddn14758 <duddn14758@naver.com>
2019-03-01 17:13:41 +0900

    11066 ing

---
+++ BaekJoon/DP/DP/11066.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+// 파일 합치기
+using namespace std;
+
+int arr[501];
+
+int main() {
+	int t,c,buf;
+	cin >> t;
+
+	while (t-- > 0) {
+		cin >> c;
+		for (int i = 0; i < c; i++) {
+			cin >> arr[i];
+		}
+		
+	}
+	
+
+
+	return 0;
+}
