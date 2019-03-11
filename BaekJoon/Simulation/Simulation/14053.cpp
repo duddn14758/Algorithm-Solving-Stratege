c6b5bfd167d3cb86476fe4e731436ee1a02dbf74
duddn14758 <duddn14758@naver.com>
2019-03-11 18:44:05 +0900

    14503 ing

--- BaekJoon/Simulation/Simulation/14053.cpp
+++ BaekJoon/Simulation/Simulation/14053.cpp
@@ -3,17 +3,17 @@
 // 로봇 청소기
 using namespace std;
 
+struct machine {
+	int x;
+	int y;
+	int dir;
+};
 bool sector[51][51];
 bool clean[51][51];
 int n, m;
 int dx[] = { -1,0,1,0 };
 int dy[] = { 0,1,0,-1 };
-
-struct pos {
-	int x;
-	int y;
-	int dir;
-};
+machine mc;
 
 bool inBoundary(int x, int y) {
 	if (x<0 || y<0 || x>n - 1 || y>n - 1)
@@ -21,29 +21,58 @@ bool inBoundary(int x, int y) {
 	return 1;
 }
 
-int dfs(int x,int y,int dir) {
-	clean[x][y] = 1;
-	
-	for (int i = 0; i < 4; i++) {
-		int xx = x + dx[i];
-		int yy = y + dy[i];
-		if (inBoundary(xx, yy)) {
-			dfs(xx, yy, i);
+void Print() {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cout << clean[i][j];
+		}
+		cout << "\n";
+	}
+	cout << "\n";
+}
+
+int run() {
+	int cnt = 0;
+	while (1) {
+		if (!clean[mc.x][mc.y]) {
+			clean[mc.x][mc.y] = 1;	// 청소
+			cnt++;
+			//Print();
+		}
+		bool back = 1;
+		for (int i = 0; i < 4; i++) {	// 4방향 검사
+			mc.dir = (mc.dir + 3) % 4;
+			int xx = mc.x + dx[mc.dir];
+			int yy = mc.y + dy[mc.dir];
+			if (inBoundary(xx, yy) && !sector[xx][yy] && !clean[xx][yy]) { // 이동
+				mc.x = xx;
+				mc.y = yy;
+				back = 0;
+				break;
+			}
+		}
+		if (back) {		// 4방항 검사 후 갈곳이없을 경우
+			int bx = mc.x+dx[(mc.dir + 2) % 4];
+			int by = mc.y+dy[(mc.dir + 2) % 4];
+			if (!inBoundary(bx, by)||sector[bx][by]) return cnt;	// 뒤가 벽이거나 밖일경우 카운트리턴
+			else {		// 벽이아니라면
+				mc.x = bx;		// 방향은그대로 뒤로 이동
+				mc.y = by;
+			}
 		}
 	}
-	return 1;
 }
 
 int main() {
 	cin >> n >> m;
-	pos start;
+	int stx,sty,stdir;
 
-	cin >> start.x >> start.y >> start.dir;
+	cin >> mc.x >> mc.y >> mc.dir;
 
 	for (int i = 0; i < n; i++)
 		for (int j = 0; j < m; j++)
 			cin >> sector[i][j];
-
+	cout << run();
 
 	return 0;
 }
