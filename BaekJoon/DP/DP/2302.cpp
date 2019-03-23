1499dd8266a70ac64072ffa0d5b52b4dc53ff0fc
DESKTOP-O257RHD\Youngwoo <duddn14758@gmail.com>
2019-03-23 23:10:23 +0900

    solve 2302 dp

--- BaekJoon/DP/DP/2302.cpp
+++ BaekJoon/DP/DP/2302.cpp
@@ -2,14 +2,28 @@
 
 // ±ØÀå ÁÂ¼®
 using namespace std;
-int vip[40];
+int vip[41];
+int dp[41];
 
 int main() {
 	int n,vp;
 	cin >> n >> vp;
-	for (int i = 0; i < vp; i++) {
+	vip[0] = 0;
+	for (int i = 1; i <= vp; i++) {
 		cin >> vip[i];
 	}
+	dp[0] = 1;
+	dp[1] = 1;
+	dp[2] = 2;	
+	for (int i = 3; i <= n+1; i++) {
+		dp[i] = dp[i - 2] * 2 + dp[i-3];
+	}
+	int org = 1;	
+	for (int i = 0; i < vp; i++) {
+		org *= dp[vip[i + 1] - vip[i]-1];
+	}
+	if (n - vip[vp] > 0) org *= dp[n - vip[vp]];
+	cout << org;
 
 	return 0;
 }
