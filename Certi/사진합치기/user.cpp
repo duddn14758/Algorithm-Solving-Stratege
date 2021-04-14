faacabd1d6099182e589f3fc2bc1229909ce9fe0
duddn14758 <duddn14758@naver.com>
2021-04-14 21:54:35 +0900

    .

--- Certi/사진합치기/user.cpp
+++ Certi/사진합치기/user.cpp
@@ -13,7 +13,7 @@ typedef struct hash {
 typedef struct photo {
 	int id;
 	int x, y;
-	bool matched = false;
+	bool matched;
 	int row[MAX_PIECE_SIZE];	// 아랫쪽
 	int col[MAX_PIECE_SIZE];	// 오른쪽
 }photo;
@@ -23,6 +23,7 @@ hash colhashPool[MAX_HASH];
 photo photoList[MAXN];
 int map[MAX_PICTURE_SIZE][MAX_PICTURE_SIZE];
 int n, m, k;
+int gNum = 0;
 
 extern bool setPicture(int id, int x, int y);
 
@@ -34,10 +35,20 @@ int getHashKey(char c[]) {
 	return val;
 }
 
+void init() {
+	for (int i = 0; i < MAX_HASH; i++) {
+		rowhashPool[i].val = 0;
+		colhashPool[i].val = 0;
+	}
+	for (int i = 0; i < MAXN; i++) {
+		photoList[i].matched = false;
+	}
+	gNum = 0;
+}
+
 void insertHashmap(int key, int id, int val, int num, int rc) {
 	if (rc == 0) {
-		while (rowhashPool[key].id != 0) {
-			printf("%d exist\n", key);
+		while (rowhashPool[key].val != 0) {
 			key = (key + 1) % MAX_HASH;
 		}
 		rowhashPool[key].id = id;
@@ -45,9 +56,7 @@ void insertHashmap(int key, int id, int val, int num, int rc) {
 		rowhashPool[key].num = num;
 	}
 	else {
-		while (colhashPool[key].id != 0) {
-			if (id == 2 && num == 1)
-				printf("@@@@@@@@@ %d exist\n", key);
+		while (colhashPool[key].val != 0) {
 			key = (key + 1) % MAX_HASH;
 		}
 		colhashPool[key].id = id;
@@ -58,16 +67,16 @@ void insertHashmap(int key, int id, int val, int num, int rc) {
 
 void matching(int id) {	// matching 안되있는 것들중에 id랑 맞는것
 	// hash에서 해당 row나 col과 같은것을 찾는다 (id의 4방향 가장자리)
-	printf("call\n");
+	//printf("gNum : %d\n", gNum + 1);
+	setPicture(id, photoList[id].y, photoList[id].x);
+	if (++gNum == n) return;
 
 	int hashKey = photoList[id].row[0] % MAX_HASH;
 	int orgKey = hashKey;
-	int col = photoList[id].col[0] % MAX_HASH;
 	bool existFlag = true;
 	hash *pic = &rowhashPool[hashKey];
 
-	if (photoList[pic->id].x != 0) {
-		printf("loop 1\n");
+	if (photoList[id].x != 0) {
 		while (pic->id == id || pic->val != photoList[id].row[0]) {
 			hashKey = (hashKey + 1) % MAX_HASH;
 			if (hashKey == orgKey) {
@@ -79,7 +88,9 @@ void matching(int id) {	// matching 안되있는 것들중에 id랑 맞는것
 
 		if (!photoList[pic->id].matched && existFlag) {
 			photoList[pic->id].matched = 1;
-			setPicture(pic->id, photoList[pic->id].x + pic->num - m, photoList[pic->id].y);
+			photoList[pic->id].x = photoList[id].x - pic->num;
+			photoList[pic->id].y = photoList[id].y;
+			//printf("up %d - (%d, %d)\n", pic->id, photoList[pic->id].x, photoList[pic->id].y);
 			matching(pic->id);
 		}
 	}
@@ -89,10 +100,8 @@ void matching(int id) {	// matching 안되있는 것들중에 id랑 맞는것
 	pic = &rowhashPool[hashKey];
 	existFlag = true;
 
-	if (photoList[pic->id].x + m != k - 1) {
-		printf("loop 2\n");
+	if (photoList[id].x + m != k) {
 		while (pic->id == id || pic->val != photoList[id].row[m - 1]) {
-			//printf("(%d - %d) (%d - %d)\n",pic->id,id,pic->val, photoList[id].row[m-1]);
 			hashKey = (hashKey + 1) % MAX_HASH;
 			if (hashKey == orgKey) {
 				existFlag = false;
@@ -103,68 +112,70 @@ void matching(int id) {	// matching 안되있는 것들중에 id랑 맞는것
 
 		if (!photoList[pic->id].matched && existFlag) {
 			photoList[pic->id].matched = 1;
-			setPicture(pic->id, photoList[pic->id].x + pic->num, photoList[pic->id].y);
+			photoList[pic->id].x = photoList[id].x + m - pic->num - 1;
+			photoList[pic->id].y = photoList[id].y;
+			//printf("down %d - (%d, %d)\n", pic->id, photoList[pic->id].x, photoList[pic->id].y);
 			matching(pic->id);
 		}
 	}
 
 	hashKey = photoList[id].col[0] % MAX_HASH;
 	orgKey = hashKey;
-	pic = &rowhashPool[hashKey];
+	pic = &colhashPool[hashKey];
 	existFlag = true;
 
-	if (photoList[pic->id].y != 0) {
-		printf("loop 3\n");
+	if (photoList[id].y != 0) {
 		while (pic->id == id || pic->val != photoList[id].col[0]) {
 			hashKey = (hashKey + 1) % MAX_HASH;
 			if (hashKey == orgKey) {
 				existFlag = false;
 				break;
 			}
-			pic = &rowhashPool[hashKey];
+			pic = &colhashPool[hashKey];
 		}
 
 		if (!photoList[pic->id].matched && existFlag) {
 			photoList[pic->id].matched = 1;
-			setPicture(pic->id, photoList[pic->id].x, photoList[pic->id].y + pic->num - m);
+			photoList[pic->id].x = photoList[id].x;
+			photoList[pic->id].y = photoList[id].y - pic->num;
+			//printf("left %d - (%d, %d)\n", pic->id, photoList[pic->id].x, photoList[pic->id].y);
 			matching(pic->id);
 		}
 	}
 
 	hashKey = photoList[id].col[m - 1] % MAX_HASH;
 	orgKey = hashKey;
-	pic = &rowhashPool[hashKey];
+	pic = &colhashPool[hashKey];
 	existFlag = true;
 
-	if (photoList[pic->id].y + m != k - 1) {
-		printf("loop 4\n");
+	if (photoList[id].y + m != k) {
 		while (pic->id == id || pic->val != photoList[id].col[m - 1]) {
 			hashKey = (hashKey + 1) % MAX_HASH;
 			if (hashKey == orgKey) {
 				existFlag = false;
 				break;
 			}
-			pic = &rowhashPool[hashKey];
+			pic = &colhashPool[hashKey];
 		}
-		printf("pic->id : %d\n",pic->id);
 
 		if (!photoList[pic->id].matched && existFlag) {
 			photoList[pic->id].matched = 1;
-			printf("Before Set\n");
-			setPicture(pic->id, photoList[pic->id].x, photoList[pic->id].y + pic->num);
-			printf("After Set\n");
+			photoList[pic->id].x = photoList[id].x;
+			photoList[pic->id].y = photoList[id].y + m - pic->num - 1; 
+			//printf("right %d - (%d, %d)\n", pic->id, photoList[pic->id].x, photoList[pic->id].y);
 			matching(pic->id);
 		}
 	}
-	printf("out\n");
+	//printf("out\n");
 }
 
 void mergePictures(int N, int M, int K, char pictures[MAXN][MAX_PIECE_SIZE][MAX_PIECE_SIZE]) {
 	int rowkey;
 	int colkey;
-	char rowbuf[4];
-	char colbuf[4];
+	char rowbuf[MAX_PIECE_SIZE];
+	char colbuf[MAX_PIECE_SIZE];
 	n = N, m = M, k = K;
+	init();
 
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < M; j++) {
@@ -176,9 +187,6 @@ void mergePictures(int N, int M, int K, char pictures[MAXN][MAX_PIECE_SIZE][MAX_
 			photoList[i].col[j] = getHashKey(colbuf);
 			insertHashmap(photoList[i].row[j] % MAX_HASH, i, photoList[i].row[j], j, 0);
 			insertHashmap(photoList[i].col[j] % MAX_HASH, i, photoList[i].col[j], j, 1);
-			if (i == 2 && j == 1) {
-				printf("colbuf : %d\n", getHashKey(colbuf));
-			}
 		}
 	}
 	photoList[0].matched = true;
